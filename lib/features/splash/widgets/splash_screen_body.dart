d068dab270d15630e1f1933ca28734540e99633a
moataz399 <moatazzezo41@gmail.com>
2023-11-19 15:01:23 +0200

    add flutter native splash screen

---
+++ lib/features/splash/widgets/splash_screen_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class SplashScreenBody extends StatelessWidget {
+  const SplashScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('splash screen '),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,6 @@ import 'package:base_project/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(DocApp(appRouter: AppRouter()));
 }
